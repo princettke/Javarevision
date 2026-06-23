cc84fae4eac29aa7bc31359c87cd673e2034309c
princettke <kushwahprince486@gmail.com>
2026-06-23 22:50:33 +0530

    commit 1 file to main

---
+++ JAVA/Advancepattern_1.java
@@ -0,0 +1,16 @@
+public class Advancepattern_1 {
+  public static void main (String []args ){
+    int n =5;
+    // upper half
+    for(int i=1;i<=n;i++){
+      // 1st part 
+      for(int j=1;j<=i;j++){
+        System.out.print("*");
+        
+
+      }
+    } 
+
+  }
+  
+}
