7179fd93121f074f1b3e9e8dbb7b33ee97b109c2
princettke <kushwahprince486@gmail.com>
2026-06-25 23:47:01 +0530

    commit 2 files to main

--- JAVA/Multiply2_no_inFunction.java
+++ JAVA/Multiply2_no_inFunction.java
@@ -1,18 +1,33 @@
 import java.util.*;
 public class Multiply2_no_inFunction {
-  public static int printMulti( int a ,int b){
-    int multi = a*b;
-    return multi;
-
+  public static int Multi(int a , int b){
+  return a*b;
   }
-  public static void main (String [] args){
+
+  public static void main (String[] args){
     Scanner sc = new Scanner(System.in);
     int a = sc.nextInt();
     int b = sc.nextInt();
-    int multi = printMulti(a,b);
-    System.out.println("multiply of two number :"+ multi);
-    sc.close();
-
+    System.out.println("multiply of two numbers is "+ Multi(a,b));
   }
-  
+
+
 }
+//   public static int printMulti( int a ,int b){
+//     //int multi = a*b;
+//     //return multi;
+
+//   }
+//   public static void main (String [] args){
+//     Scanner sc = new Scanner(System.in);
+//     int a = sc.nextInt();
+//     int b = sc.nextInt();
+//     int multi = printMulti(a,b);
+//     System.out.println("multiply of two number :"+ multi);
+//     sc.close(); //maine isliye add kiya tha kyunki Scanner object ko use karne ke baad close karna ek good practice mana jata hai.
+
+    
+
+//   }
+  
+// }
